e96ed17cf3be0a3ad66f3ec28ef0cfc1edc2e553
Henry Wang <henrwang@visa.com>
2017-09-13 14:04:37 -0700

    AccountRestTest + AccountController changed

---
+++ src/main/java/com/visa/ncg/canteen/AccountResponse.java
@@ -0,0 +1,22 @@
+package com.visa.ncg.canteen;
+
+public class AccountResponse {
+    private int balance;
+    private long id;
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
